73663607121f930da00c089f33a3d5747e4c6bbd
Vitor de Paula <vitordcode@gmail.com>
2023-01-17 13:13:32 -0300

    Botoes e avatar inserido

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
+import { GithubLogo, QrCode, LinkedinLogo } from 'phosphor-react'
 
 export function App() {
-
   return (
-    <div>
-      Links
+    <div className="container">
+      <img
+        className="avatar"
+        src="https://github.com/vitordcode.png"
+        alt="Vitor de Paula avatar"
+      />
+      <span>@vitordpaula1</span>
+
+      <a
+        className="social-links"
+        href="https://github.com/vitordcode"
+        target="_blank"
+      >
+        <GithubLogo size={24} />
+        <span>Github</span>
+      </a>
+
+      <a
+        className="social-links"
+        href="https://vitordcode.com.br"
+        target="_blank"
+      >
+        <QrCode size={24} />
+        <span>Portfolio</span>
+      </a>
+
+      <a
+        className="social-links"
+        href="https://linkedin.com/vitordcode"
+        target="_blank"
+      >
+        <LinkedinLogo size={24} />
+        <span>Linkdin</span>
+      </a>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 
-import { GlobalStyle } from './Global.css'
+import './Global.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <>
-      <GlobalStyle />
-
-      <App />
-    </>
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 )
